05207d52b12d938fa6c821e92c102a2f16be55f0
Fahim Shariar <fahimshariar28@gmail.com>
2023-03-03 04:14:12 +0600

    Modal 40% Complete

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,6 @@ const loadContent = async () => {
 const displayContent = (contents) => {
   const contentContainer = document.getElementById("content-container");
   contents.tools.forEach((content) => {
-    console.log(contents.tools);
     const contentDiv = document.createElement("div");
     contentDiv.classList.add("col");
     contentDiv.innerHTML = `
@@ -39,12 +38,99 @@ const displayContent = (contents) => {
               }</i></p>
               </div>
               <div>
-                <button class="border-0"><i class="fa-solid fa-arrow-right"></i></button>
+                <button onclick="loadContentDetails('${
+                  content.id
+                }')" class="border-0" data-bs-toggle="modal" data-bs-target="#contentModal"><i class="fa-solid fa-arrow-right"></i></button>
               </div>
             </div>
           </div>
         `;
     contentContainer.appendChild(contentDiv);
   });
+};
+
+const loadContentDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayContentDetails(data.data);
+};
+const displayContentDetails = (content) => {
+  console.log(content.pricing);
+  const modalBody = document.getElementById("modal-body");
+  modalBody.innerHTML = `
+            <div
+                class="d-flex justify-content-between align-items-center gap-3"
+              >
+                <div class="w-50 bg-info-subtle rounded border border-info p-2">
+                  <h3>${content.description}</h3>
+                  <div class="d-flex gap-3">
+                    <div>
+                      <h5 class="bg-light rounded text-success text-center p-2">
+                        ${
+                          content.pricing != null
+                            ? content.pricing[0].price
+                            : "Free of cost"
+                        }
+                        ${
+                          content.pricing != null
+                            ? content.pricing[0].plan
+                            : "Basic"
+                        }
+                      </h5>
+                    </div>
+                    <div>
+                      <h5 class="bg-light rounded text-danger-emphasis text-center p-2">
+                        ${
+                          content.pricing != null
+                            ? content.pricing[1].price
+                            : "Free of cost"
+                        }
+                        ${
+                          content.pricing != null
+                            ? content.pricing[1].plan
+                            : "Pro"
+                        }
+                      </h5>
+                    </div>
+                    <div>
+                      <h5 class="bg-light rounded text-danger text-center p-2">
+                        ${
+                          content.pricing != null
+                            ? content.pricing[2].price
+                            : "Free of cost"
+                        }
+                        ${
+                          content.pricing != null
+                            ? content.pricing[2].plan
+                            : "Enterprise"
+                        }
+                      </h5>
+                    </div>
+                  </div>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div>
+                      <h5>Features</h5>
+                    </div>
+                    <div>
+                      <h5>Integrations</h5>
+                    </div>
+                  </div>
+                </div>
+                <div class="w-50 border border-secondary rounded p-2">
+                  <div class="position-relative">
+                    <img class="w-100" src="${content.image_link[0]}" alt="" />
+                    <span
+                      class="badge text-bg-danger position-absolute top-0 end-0"
+                      >Accuracy</span
+                    >
+                  </div>
+                  <h5>Question</h5>
+                  <p>ans</p>
+                </div>
+            </div>
+    `;
 };
 loadContent();
